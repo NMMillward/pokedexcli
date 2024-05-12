68a6d4b5fb1119fa884a7d77accd0bd1d7960840
Nick Millward <Nick@titanproducts.com>
2024-05-12 08:28:23 +0100

    3. REPL

--- main.go
+++ main.go
@@ -1,9 +1,72 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
+}
+
 func main() {
-	fmt.Println("Hello, world!")
+	commands := getCommands()
+	willExit := false
+
+	for {
+		fmt.Print("Pokedex > ")
+		input := bufio.NewScanner(os.Stdin)
+		input.Scan()
+
+		switch input.Text() {
+		case "exit":
+			willExit = true
+			break
+		default:
+			if command, ok := commands[input.Text()]; ok {
+				if err := command.callback(); err != nil {
+					break
+				}
+			} else {
+				_ = commandHelp()
+			}
+		}
+
+		if willExit {
+			break
+		}
+	}
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:")
+	fmt.Println()
+	commands := getCommands()
+	for _, command := range commands {
+		fmt.Println(fmt.Sprintf("%s: %s", command.name, command.description))
+	}
+	return nil
+}
+
+func commandExit() error {
+	return nil
 }
